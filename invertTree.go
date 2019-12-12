8f6b5909ea98558c8ee7e56b1a8504d9c9f63f28
wuTree <664065745@qq.com>
2019-12-12 10:44:30 +0800

    添加 1、树翻转 2、机器人 格子平面 移动 原点

---
+++ invertTree.go
@@ -0,0 +1,39 @@
+package main
+
+/**
+翻转一棵二叉树。
+
+示例：
+
+输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+func main() {
+
+	root := &TreeNode{4, &TreeNode{2, &TreeNode{Val: 1}, &TreeNode{Val: 3}}, &TreeNode{7, &TreeNode{Val: 6}, &TreeNode{Val: 9}}}
+	invertTree(root)
+
+}
